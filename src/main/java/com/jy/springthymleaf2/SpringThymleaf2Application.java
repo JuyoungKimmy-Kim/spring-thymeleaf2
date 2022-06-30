dfa99da26133977cda44f94eadb716869ffe0568
Juyoung Kim <kimmykim.jy@gmail.com>
2022-06-30 17:01:11 +0900

    new project

---
+++ src/main/java/com/jy/springthymleaf2/SpringThymleaf2Application.java
@@ -0,0 +1,13 @@
+package com.jy.springthymleaf2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymleaf2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymleaf2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jy/springthymleaf2/SpringThymleaf2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.springthymleaf2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymleaf2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
